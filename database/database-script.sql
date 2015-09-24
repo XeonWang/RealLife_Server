0fe77ac6d9b99fb19a739f8d8f680da251c3032d
XeonWang <wangzhiqiang1026@gmail.com>
2015-09-24 18:07:09 +0800

    add password column to Uzer table

--- database/database-script.sql
+++ database/database-script.sql
@@ -2,5 +2,6 @@
 CREATE Uzer (
 	id varchar(10),
 	name varchar(20),
+	password char(32); /* It will store a 32byte MD5 encoding password */
 	friends varchar(1000), /* Set friends length as 1000 temperary, support max friends number is 90 */
 );
