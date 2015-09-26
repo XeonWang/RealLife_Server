8958323f0356c0b2b5efc8901eccb6e8c0e4b465
Jason Wang <wangzhiqiang1026@gmail.com>
2015-09-26 10:32:36 +0800

    Remove files do not need anymore

--- database/database-script.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE Uzer (
-	id varchar(10),
-	name varchar(20),
-	password char(32), /* It will store a 32byte MD5 encoding password */
-	friends varchar(1000) /* Set friends length as 1000 temperary, support max friends number is 90 */
-);
-
-ALTER TABLE Uzer ADD CONSTRAINT PK_Uzer PRIMARY KEY Uzer(id);
